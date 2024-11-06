57b2d41492cd75fca1a3dd1f68a0402488939d5f
vimossi <vini_agg@hotmail.com>
2024-11-06 14:54:13 -0400

    resolve cypress functionality error

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,29 @@
+import userData from '../fixtures/user-data.json'
+
+describe('Orange HRM Tests', () => {
+  const selectorList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    wrongCredentialAlert: "[role='alert']",
+    dashboardGrid: ".orangehrm-dashboard-grid"
+  }
+
+  it('User info update', () => {
+    cy.visit('auth/login')
+    cy.get(selectorList.usernameField).type(userData.userSucess.username)
+    cy.get(selectorList.passwordField).type(userData.userSucess.password)
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorList.dashboardGrid)
+    cy.get('[href="/web/index.php/pim/viewMyDetails"]').click()
+  })
+
+  it('Login sem sucesso', () => {
+    cy.visit('auth/login')
+    cy.get(selectorList.usernameField).type(userData.userFail.username)
+    cy.get(selectorList.passwordField).type(userData.userFail.password)
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert)
+  })
+})
